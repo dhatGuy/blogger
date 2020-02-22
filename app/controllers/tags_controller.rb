e0064557134f36ee82df856cab4ea73c10964640
dhatguy <odunsiolakunbi@gmail.com>
2020-02-22 21:57:59 +0100

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,21 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tag = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    if @tag.destroy
+      flash.notice = "'#{@tag.name}' tag successfully deleted."
+      redirect_to tags_path
+    else
+      flash.notice = 'Something went wrong'
+      redirect_to tags_path
+    end
+  end
+  
+end
